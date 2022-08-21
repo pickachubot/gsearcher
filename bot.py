e03e3784893185c9b1cefa1060e16aada0350613
pickachubot <106468636+pickachubot@users.noreply.github.com>
2022-08-22 02:50:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
-from utils import temp
+from main import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
 
